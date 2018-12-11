17e11c5c39e8f1bb8478aa97087d6f850b9b7393
Andreas Eismann <abulvenz@gmail.com>
2018-12-11 23:41:40 +0100

    Moved the rendering from SVG to canvas API. Speed is incredible, now the text has to be converted as well

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import m from "mithril";
 import tagl from "tagl-mithril";
 import fn from "./fn";
 
-const { svg, circle, text, g } = tagl(m);
+const { canvas } = tagl(m);
 
 let space_width = innerWidth * 15;
 let space_height = innerHeight * 15;
 let space_depth = 100;
 let frust = 0.3;
 
-let numberOfStars = 404;
+let numberOfStars = 2040;
+
+let rrr = 0;
 
 //* Set to 0 for no fade, set to 1 for fade in effect. */
 let darkness = 1;
@@ -48,9 +50,9 @@ const star = () => {
     z: Math.random() * space_depth + 1
   });
   let _vel = point({
-    x: 0 * (Math.random() * 1 - 0.5),
-    y: 0 * (Math.random() * 1 - 0.5),
-    z: -0.5
+    x: rrr * (Math.random() * 1 - 0.5),
+    y: rrr * (Math.random() * 1 - 0.5),
+    z: rrr * Math.random() - 0.5
   });
   return {
     pos: _pos,
@@ -76,6 +78,50 @@ const iterate = () => {
 
 setInterval(iterate, 50);
 
+let mypos = () => {
+  x, y, z, rx, ry, rz;
+};
+
+const vMat = ({ x, y, z, rx, ry, rz }) => {
+  let ca = Math.cos(rz);
+  let cb = Math.cos(ry);
+  let cc = Math.cos(rx);
+  let sa = Math.sin(rz);
+  let sb = Math.sin(ry);
+  let sc = Math.sin(rx);
+
+  let rows = [
+    [ca * cb, ca * sb * sc - sa * cc, ca * sb * cc + sa * sc, x],
+    [sa * cb, sa * sb * sc + ca * cc, sa * sb * cc - ca * sc, y],
+    [-sb, cb * sc, cb * cc, z][(0, 0, 0, 1)]
+  ];
+
+  let a = rows[0][0]*x + rows[1][0]*y+rows[2][0]*z;
+  let b = rows[0][1]*x + rows[1][1]*y+rows[2][1]*z;
+  let c = rows[0][2]*x + rows[1][2]*y+rows[2][2]*z;
+
+  let inv = [
+      [rows[0][0],rows[1][0],rows[2][0],-a],
+      [rows[0][1],rows[1][1],rows[2][1],-b],
+      [rows[0][2],rows[1][2],rows[2][2],-c],
+  ];
+
+  return {
+    toView: ({ x, y, z }) => {
+      let result = {
+        x: inv[0][0] * x + inv[0][1] * y + inv[0][2] * y - inv[0][3],
+        y: inv[1][0] * x + inv[1][1] * y + inv[1][2] * y - inv[1][3],
+        z: inv[2][0] * x + inv[2][1] * y + inv[2][2] * y - inv[2][3]
+      };
+
+      return result;
+    }
+  };
+};
+
+const grad2rad = a => a / 180 * Math.PI;
+
+
 class Star {
   view(vnode) {
     let s = vnode.attrs.star;
@@ -96,55 +142,38 @@ class Star {
 }
 
 class Space {
+  oncreate(vnode) {
+    console.log(vnode.dom);
+    this.ctx = vnode.dom.getContext("2d");
+    this.clear();
+  }
+  clear() {
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, innerWidth, innerHeight);
+  }
+  onbeforeupdate(vnode) {
+    this.clear();
+
+    stars.forEach(s => {
+      this.ctx.beginPath();
+
+      let z__ = 15 / s.pos.z();
+      if (s.pos.z() < 1) s.pos = point(s.pos.x(), s.pos.y(), 100);
+      let cx =
+        (s.pos.x() - innerWidth * 0.5) / s.pos.z() / frust + innerWidth * 0.5;
+      let cy =
+        (s.pos.y() - innerHeight * 0.5) / s.pos.z() / frust + innerHeight * 0.5;
+      this.ctx.arc(cx, cy, z__, 0, 2 * Math.PI);
+      this.ctx.fillStyle = "white";
+      this.ctx.fill();
+    });
+  }
+
   view(vnode) {
-    return svg(
-      {
-        width: innerWidth,
-        height: innerHeight,
-        style: `background-color:rgb(${darkness * 255},${darkness *
-          255},${darkness * 255})`
-      },
-      stars.map((star, idx) => m(Star, { key: idx, star: star })),
-      g(
-        {},
-        text(
-          {
-            x: "50%",
-            y: "50%",
-            style: "font: bold 160px sans-serif;",
-            stroke: "lightgray",
-            "stroke-opacity": 0.3,
-            "text-anchor": "middle",
-            "fill-opacity": 0
-          },
-          "404"
-        ),
-        text(
-          {
-            x: "50%",
-            y: "75%",
-            style: "font: bold 36px sans-serif;",
-            stroke: "lightgray",
-            "stroke-opacity": 0.3,
-            "text-anchor": "middle",
-            "fill-opacity": 0
-          },
-          "Empty Space"
-        ),
-        text(
-          {
-            x: "50%",
-            y: "15%",
-            style: "font: bold 36px sans-serif;",
-            stroke: "lightgray",
-            "stroke-opacity": 0.3,
-            "text-anchor": "middle",
-            "fill-opacity": 0
-          },
-          title || "Empty Space"
-        )
-      )
-    );
+    return canvas.$canvas({
+      width: innerWidth,
+      height: innerHeight
+    });
   }
 }
 
